90a5dc23120a852acddff80d59d9f04daa27a58e
Gereltbaatar <mglgerelt@gmail.com>
2024-09-24 13:24:00 +0800

    yronhii ymaa hiisen

--- G_T/project-back-end/server.js
+++ G_T/project-back-end/server.js
@@ -1,1 +1,18 @@
-console.log("neeh");
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 8111;
+const server = express();
+server.use(bodyParser.text());
+
+server.get("/", (request, response) => {
+  response.send("hello world");
+});
+
+server.post("/", (request, response) => {
+  response.send(`ene tanii --- ${request.body} ---`);
+});
+
+server.listen(port, () => {
+  console.log(`server ajilah http://localhost:${port}`);
+});
