f8191dbb9c04a30baa45cfbd1602e7212e0bd3cd
Энх-Амар Болдбаатар <LPC240855@G401-CC0159.local>
2024-09-25 11:47:25 +0800

    sign in sign up backend

--- team-se/back-end/server.js
+++ team-se/back-end/server.js
@@ -19,24 +19,82 @@
 // server.listen(port, () => {
 //   console.log(`server ajillaa http://localhost:${port}`);
 // });
-
+import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
-const port = 3000;
+import fs from "fs";
+import { error } from "console";
+
+const port = 8000;
+
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
+
 app.get("/", (request, response) => {
   response.send("hi get tanii huselt");
 });
 
-app.use((req, res) => {
-  res.status(404);
-  res.send(`<h1>Error 404!</h1>`);
+
+app.post("/", (request, response) => {
+  const { name, body } = request.body;
+  fs.readFile("./data/user.json", "utf-8", (readError, data) => {
+    readError && response.json({ succes: false, error: error });
+  });
+
+  let savedData = data ? JSON.parse(data) : [];
+
+  const registeredUser = savedData.filter(
+    (user) => user.name === name && user.password === password
+  );
+  registeredUser.length
+    ? response.json({
+        success: true,
+        user: registeredUser[0],
+      })
+    : response.json({
+        success: false,
+      });
 });
 
-app.post("/register", (request, response) => {
-  console.log(request.body);
-  response.send(`hi post tanii huselt`);
+app.post("/sign-in", (request, response) => {
+  const { name, email, password } = request.body;
+
+  fs.readFile("./data/user.json", "utf-8", (readError, data) => {
+    let savedData = data ? JSON.parse(data) : [];
+
+    if (readError) {
+      response.json({
+        success: false,
+        error: error,
+      });
+    }
+
+    console.log(data);
+
+    const newUser = {
+      id: Date.now().toString(),
+      name: name,
+      email: email,
+      password: password,
+    };
+    savedData.push(newUser);
+
+    fs.writeFile("./data/user.json", JSON.stringify(savedData), (error) => {
+      if (error) {
+        response.json({
+          success: false,
+          error: error,
+        });
+      } else {
+        response.json({
+          success: true,
+          user: newUser,
+        });
+      }
+    });
+  });
 });
 
 app.listen(port, () => {
