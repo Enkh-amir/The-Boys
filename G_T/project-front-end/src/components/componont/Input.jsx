54cb0cb48ab2c1604af273b8202c667f8bdf9840
Түвшин Эрдэнэбат <24LP0845@G401-CC0091.local>
2024-09-25 09:06:44 +0800

    signinpage-hiiv

--- G_T/project-front-end/src/components/componont/Input.jsx
+++ G_T/project-front-end/src/components/componont/Input.jsx
@@ -1,10 +1,10 @@
-export const Input = ({ placeholder }) => {
+export const Input = ({ placeholder, type }) => {
   return (
-    <div className="text-base not-italic font-normal rounded-lg border border-[#D1D5DB] py-2 px-4 flex items-center self-stretch bg-[#F3F4F6] text-[#A3A3A3]  ">
+    <div className="text-base not-italic font-normal rounded-lg border border-[#D1D5DB] py-2 px-4 flex items-center self-stretch bg-[#F3F4F6] text-[#A3A3A3]">
       <input
-        type="text"
+        type={type}
         placeholder={placeholder}
-        className="bg-[#F3F4F6] w-full"
+        className="bg-[#F3F4F6] w-full outline-none"
       />
     </div>
   );
