af2bca938204ac9e0ed42f33153c169a8b1e0049
Ганжаргал Гантөмөр <24LP0756@G303-CC0140.local>
2024-10-01 20:36:21 +0800

    uptede

--- temka-ganjaa/project-2/front-end/src/components/pages/Home.jsx
+++ temka-ganjaa/project-2/front-end/src/components/pages/Home.jsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from "react/cjs/react.production.min";
+"use client";
+import { useState, useEffect } from "react";
 import { Cards } from "../component/Cards";
 import { Create } from "../component/Create";
 
 export const HomePage = () => {
   const [product, setProduct] = useState([]);
-  const BACKEND_ENDPOINT = "http://localhost:8000/sign-up"; // Updated to the correct endpoint
-  try {
-     const fetchProduct = async () => {
-    const response = await fetch(BACKEND_ENDPOINT);
-    const responseData = await response.json();
-    setProduct(responseData);
+  const BACKEND_ENDPOINT = "http://localhost:8888"; // Updated to the correct endpoint
+  const fetchdata = async () => {
+    try {
+      const response = await fetch(BACKEND_ENDPOINT);
+      const responseData = await response.json();
+      setProduct(responseData.product);
+    } catch (error) {
+      console.log(error);
+    }
   };
-  } catch (error){
-console.log(error);
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  console.log(product);
 
-  }
- 
-
-  useEffect(fetchProduct(), []);
   const handleOnSubmit = async (event) => {
     event.preventDefault();
 
-    const userData = {
-      name: event.target.name.value,
-      email: event.target.email.value,
-      password: event.target.password.value,
-    };
 
     const options = {
       method: "POST",
@@ -39,14 +36,6 @@ console.log(error);
       const response = await fetch(BACKEND_ENDPOINT, options);
       const data = await response.json();
       console.log(data);
-
-      if (data.success) {
-        // Handle successful signup (e.g., redirect or show message)
-        console.log("User signed up successfully:", data.user);
-      } else {
-        // Handle signup failure (e.g., show error message)
-        console.error("Signup failed:", data.error);
-      }
     } catch (error) {
       console.error("Error submitting form:", error);
     }
@@ -55,11 +44,14 @@ console.log(error);
     <div className="w-full h-screen flex justify-center items-center relative">
       <Create />
       <div className="max-w-[1546px] w-full h-auto flex flex-wrap gap-[30px]">
+        {product?.map((product) => {
+          return <Cards key={product?.id} product={product} />;
+        })}
+
+        {/* <Cards />
         <Cards />
         <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
+        <Cards /> */}
       </div>
     </div>
   );
